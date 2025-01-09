2f0459a53cd72e09ba0d9c9b439f5da6d301c79d
vihanvt <vihan.vnt@gmail.com>
2025-01-09 16:09:42 +0530

    practice

--- class2.cpp
+++ class2.cpp
@@ -3,7 +3,7 @@ using namespace std;
 class BankAccount{
 public:
     int balance,deposited,withdrew;//property of the class
-    void deposit(){//method of a class
+    void deposit(){//method of a class=function in a class
         balance+=deposited;
         cout<<"The balance after deposit is"<<balance<<endl;
     }
@@ -16,7 +16,7 @@ public:
     }
 };
 int main(){
-    BankAccount bank;//object of a class 
+    BankAccount bank;//object of the class
     bank.balance=500;
     bank.deposited=200;
     bank.withdrew=100;
